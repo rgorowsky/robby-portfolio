74bb003c463eb91845d521a6fceb13e7881f2cca
rgorowsky <robert.gorowsky@sportngin.com>
2024-12-17 04:08:31 -0600

    updated remaining methods

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ setInterval(getCentralTime, 1000);
 
 async function getUtcTime() {
   let quickTime = new Promise((resolve) => {
-    const options = { timeZone: 'utc', hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const options = { timeZone: 'UTC', hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' };
     const centralTime = new Date().toLocaleString('en-US', options);
     resolve("UTC Standard Time " + quickTime);
   });
@@ -70,18 +70,15 @@ async function getUtcTime() {
 getUtcTime();
 setInterval(getUtcTime, 1000);
 
-async function getNewRelicTime() {
+  async function getNewRelicTime() {
+    let quickTime = new Promise((resolve) => {
+      const options = { timeZone: 'UTC', hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' };
+      const centralTime = new Date().toLocaleString('en-US', options);
+      resolve("New Relic App Time " + newRelicTime);
+    });
 
-  let quickTime = new Promise(function(resolve,reject) {
-
-    var start_date = new Date();
-    var utc_offset = start_date.setUTCHours(28); // this has been off 1 day behind, adding 24 hours to fix offset
-    var utc_time = new Date(utc_offset).toLocaleString();
-
-    resolve("New Relic App Time " + utc_time);
-  })
-  document.getElementById("newrelic_time").innerHTML = await quickTime
-}
+    document.getElementById("newrelic_time").innerHTML = await quickTime;
+  }
 
 getNewRelicTime();
 setInterval(getNewRelicTime, 1000);
@@ -103,32 +100,27 @@ getSplunkTime();
 setInterval(getSplunkTime, 1000);
 
 async function getColoradoTime() {
+  let quickTime = new Promise((resolve) => {
+    const options = { timeZone: 'America/Denver', hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const coloradoTime = new Date().toLocaleString('en-US', options);
+    resolve("Denver Mountain Daylight Time " + coloradoTime);
+  });
 
-  let quickTime = new Promise(function(resolve,reject) {
-
-    var start_date = new Date();
-    var utc_offset = start_date.setUTCHours(27); // time has been behind by 1 day, adding 24 hours to offset
-    var utc_time = new Date(utc_offset).toLocaleString();
-
-    resolve("Denver Mountain Daylight Time " + utc_time);
-  })
-  document.getElementById("colorado_time").innerHTML = await quickTime
+  document.getElementById("colorado_time").innerHTML = await quickTime;
 }
 
 getColoradoTime();
 setInterval(getColoradoTime, 1000);
 
+
 async function getFloridaTime() {
+  let quickTime = new Promise((resolve) => {
+    const options = { timeZone: 'America/New_York', hour12: true, hour: '2-digit', minute: '2-digit', second: '2-digit' };
+    const floridaTime = new Date().toLocaleString('en-US', options);
+    resolve("Florida Eastern Daylight Time " + floridaTime);
+  });
 
-  let quickTime = new Promise(function(resolve,reject) {
-
-    var start_date = new Date();
-    var utc_offset = start_date.setUTCHours(22); // florida time 12/22 4pm - coming in as 12/21 11 pm - adjust offset from 5 (+17) to 22
-    var utc_time = new Date(utc_offset).toLocaleString();
-
-    resolve("Florida Eastern Daylight Time " + utc_time);
-  })
-  document.getElementById("florida_time").innerHTML = await quickTime
+  document.getElementById("florida_time").innerHTML = await quickTime;
 }
 
 getFloridaTime();
