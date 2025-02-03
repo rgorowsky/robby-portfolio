3eb36badd25034064d8367ea26b2e9bc83fc8c17
rgorowsky <robert.gorowsky@sportngin.com>
2025-02-03 01:41:07 -0600

    changed import to const in server file and added type module to package

--- lambda-deploy/server.js
+++ lambda-deploy/server.js
@@ -1,5 +1,4 @@
-import AWS from "aws-sdk";
-
+const AWS = require("aws-sdk");
 const ses = new AWS.SES({ region: "us-east-2" });
 
 exports.handler = async (event: any) => {
@@ -19,6 +18,11 @@ exports.handler = async (event: any) => {
 
     return {
       statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type"
+      },
       body: JSON.stringify({ message: "Email sent successfully!" })
     };
   } catch (error) {
